7f7256f06524c178709b08ff99663c7c37fb555c
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2020-01-01 01:11:21 +0900

    models valu calc is added

--- eng_app/app/models/score_eng_not_written.rb
+++ eng_app/app/models/score_eng_not_written.rb
@@ -1,2 +1,9 @@
 class ScoreEngNotWritten < Score
+  before_create do
+    if self.passed
+      self.val = 1
+    else
+      self.val = -1
+    end
+  end
 end
--- eng_app/app/models/score_eng_written.rb
+++ eng_app/app/models/score_eng_written.rb
@@ -1,2 +1,9 @@
 class ScoreEngWritten < Score
+  before_create do
+    if self.passed
+      self.val = 2
+    else
+      self.val = -2
+    end
+  end
 end
