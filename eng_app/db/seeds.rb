04b2695d17d54fc2eecd784e52590052a6d6155b
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2019-12-31 11:33:52 +0900

    eng_app updated

--- eng_app/db/seeds.rb
+++ eng_app/db/seeds.rb
@@ -5,5 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+sentence_file_path="/home/miyakz/sentence_data.txt"
+
+unless sentence_file_path
+  puts "ERROR: sentence_file_path is required"
+  exit 
+end
+
+Sentence.destroy_all
+
+File.open(sentence_file_path) do |f|
+  f.each_line do |line|
+    s = line.split("::")
+    no = s[0].to_i
+    jp = s[1]
+    en = s[2]
+    Sentence.create([{no: no, jp: jp, en: en}])
+  end
+end
+
 
 
