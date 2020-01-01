1dc3c2498a7a09174f4be1e15f5791517c62d11c
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2020-01-01 13:32:42 +0900

    delete user model/controller/view preparing for using devise

--- eng_app/config/routes.rb
+++ eng_app/config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   resources :score_eng_writtens
   resources :scores
   resources :sentences
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- eng_app/db/schema.rb
+++ eng_app/db/schema.rb
@@ -10,31 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_31_014939) do
-
-  create_table "scores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.boolean "passed"
-    t.integer "val"
-    t.integer "sentence_id"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "sentences", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "no"
-    t.string "en"
-    t.string "jp"
-    t.string "voice_data"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "passwd_hash"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
