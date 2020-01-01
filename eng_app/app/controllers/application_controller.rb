0b4ff8b0da652c9e4f8a689083003bdbcde65bdd
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2020-01-02 01:41:38 +0900

    return referer page

--- eng_app/app/controllers/application_controller.rb
+++ eng_app/app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::Base
+  before_action :set_request_from
+
+    # どこのページからリクエストが来たか保存しておく
+  def set_request_from
+    if session[:request_from]
+      @request_from = session[:request_from]
+    end
+    # 現在のURLを保存しておく
+    session[:request_from] = request.original_url
+  end
+
+  # 前の画面に戻る
+  def return_back
+    if request.referer
+      redirect_to request.referer and return true
+    elsif @request_from
+      redirect_to @request_from and return true
+    end
+  end
+
+  def return_back_url
+    if request.referer
+      return request.referer
+    elsif @request_from
+      return @request_from
+    end
+  end
+
+
+  helper_method :return_back
+  helper_method :return_back_url
 end
