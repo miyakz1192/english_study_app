3314b908bd486ccfa41a5dd69540d72e3fc135bd
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2020-01-01 23:14:29 +0900

    first version of login/logout by devise

--- eng_app/config/routes.rb
+++ eng_app/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'accesses/hello'
+  get 'accesses/goodbye'
   resources :score_eng_not_writtens
   resources :score_eng_writtens
   resources :scores
   resources :sentences
+  root 'accesses#hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- eng_app/db/schema.rb
+++ eng_app/db/schema.rb
@@ -10,6 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_01_01_050938) do
+
+  create_table "scores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.boolean "passed"
+    t.integer "val"
+    t.integer "sentence_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sentences", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "no"
+    t.string "en"
+    t.string "jp"
+    t.string "voice_data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
