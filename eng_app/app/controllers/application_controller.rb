ee0443909b0a9de72c20c4d1bd3cea549bfebc07
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2020-01-02 13:52:44 +0900

    refactoring: some hepler method is moved from application_controller.rb to application_helpler.rb

--- eng_app/app/controllers/application_controller.rb
+++ eng_app/app/controllers/application_controller.rb
@@ -9,25 +9,4 @@ class ApplicationController < ActionController::Base
     # 現在のURLを保存しておく
     session[:request_from] = request.original_url
   end
-
-  # 前の画面に戻る
-  def return_back
-    if request.referer
-      redirect_to request.referer and return true
-    elsif @request_from
-      redirect_to @request_from and return true
-    end
-  end
-
-  def return_back_url
-    if request.referer
-      return request.referer
-    elsif @request_from
-      return @request_from
-    end
-  end
-
-
-  helper_method :return_back
-  helper_method :return_back_url
 end
