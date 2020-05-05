23bc02129cc22be920b185ada5cfce9620f4f88b
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2020-05-05 11:22:46 +0000

    eng-app-wm-app.sh added retry

--- eng_app/etc/init_other_service/eng-app-wm-app.sh
+++ eng_app/etc/init_other_service/eng-app-wm-app.sh
@@ -4,7 +4,15 @@ cat /mnt/sentence_data.txt | while read line
 do 
   sentence_no=`echo ${line} | awk -F :: '{print $1}'`
   en=`echo ${line} | awk -F :: '{print $3}'`
-  set -x 
-  curl -X POST -H "Content-Type: application/json" -d "{\"en\": \"${en}\"}"  eng-app-wm-app-service:3000/words/${sentence_no}
-  set +x
+  while :
+  do
+    set -x 
+    curl -X POST -H "Content-Type: application/json" -d "{\"en\": \"${en}\"}"  eng-app-wm-app-service:3000/words/${sentence_no}
+    if [ $? -eq 0 ]; then
+      set +x
+      break
+    fi
+    set +x
+    echo "WARNING: curl return non 0. retry it."
+  done
 done
