9b1ee431842ef2a7df723fe1d31ed312813df58f
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2020-05-09 16:19:27 +0000

    avoid Uninitialized Constant

--- eng_app/app/models/concerns/view_model_base.rb
+++ eng_app/app/models/concerns/view_model_base.rb
@@ -1,4 +1,9 @@
+
+
 module ViewModelBase
+  #require "#{Rails.root}/app/models/view_model/user_view_model.rb"
+  load "#{Rails.root}/app/models/view_model/user_view_model.rb"
+
   def view_model
     #@view_model if @view_model
     @view_model = eval "#{self.class.name}ViewModel.new(self)"
