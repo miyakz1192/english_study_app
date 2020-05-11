c4702da112bc7d511b8ccc2792c3fcc697ae05a1
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2020-05-11 14:40:49 +0000

    WordMeaningService bug fix

--- eng_app/lib/word_meaning_service.rb
+++ eng_app/lib/word_meaning_service.rb
@@ -10,6 +10,12 @@ class WordMeaningService
   def get(sentence)
     begin
       JSON.load(open("#{@uri}/#{sentence.no}").read)
+    rescue OpenURI::HTTPError => e
+      Rails.logger.error ("ERROR in WordMeaningService:get 404 ERROR")
+      #404 ERROR
+      if ([e.message].grep /404/).size != 0
+        return [{"en" => "NO WORDS REGISTERD", "note" => "NO WORDS REGISTERD", "url" => "http://"} ]
+      end
     rescue => e
       Rails.logger.error ("ERROR in WordMeaningService:get")
       Rails.logger.error ([e.message]+e.backtrace).join($/)
