6d076a406b4ff78eb72577c4bfa1e6f1aaa8aa52
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2020-06-19 15:35:15 +0000

    update grpc client

--- eng_app/client.rb
+++ eng_app/client.rb
@@ -9,39 +9,15 @@ require 'grpc'
 require 'erpc/sentence_services_pb.rb'
 
 def main
-  stub = SentenceService::Stub.new('localhost:50051', :this_channel_is_insecure)
-  u = User.new({id: "1"})
-
-  puts u.class.name
-  puts u.class.inspect
-  puts u.class.class.name
-  puts u.class.class.inspect
-  puts u.inspect
-  puts u.class.methods.grep /encode/
-  stub.list_by_worst(u) # ERRRO
-  #stub.list_by_worst({id: "1"})
-  #message = stub.list_by_worst(nil).message
-  #p "Greeting: #{message}"
-end
-
-def main2
-
   puts "start client"
   stub = Erpc::SentenceService::Stub.new('localhost:50051', :this_channel_is_insecure)
-  #u = Erpc::User.new({id: "req_from_client_1"})
-  u = Erpc::User.new({id: "1"})
+  u = Erpc::User.new({id: 1})
 
-  puts u.class.name
-  puts u.class.inspect
-  puts u.class.class.name
-  puts u.class.class.inspect
-  puts u.inspect
-  puts u.class.methods.grep /encode/
   sentence = stub.list_by_worst(u) # ERRRO
   p "output=#{sentence.inspect}"
 
   puts "end client"
 end
 
-main2
+main
 
